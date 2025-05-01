8a3f2241fe05bfe99c456ea2bf3a3f8bac9d12c4
Ayush <ayushparadkar6@gmail.com>
2025-05-01 19:42:38 +0530

    Initial commit with Reactd

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -1,8 +1,8 @@
-import first from "./4609.jpg";
-import second from "./6152022_general2_13.jpg";
-import third from "./42534.jpg";
-import fourth from "./8357605.jpg";
-import five from "./waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer.jpg";
+import first from "./4609.webp";
+import second from "./6152022_general2_13.webp";
+import third from "./42534.webp";
+import fourth from "./8357605.webp";
+import five from "./waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer.webp";
 import six from "./aboutus.webp";
 
 export const photos = {
